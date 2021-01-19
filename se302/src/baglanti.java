08567cdd3f3501ce9463226759dfb1b01c3af3bb
MerveTelli <61708140+MerveTelli@users.noreply.github.com>
2021-01-19 06:10:06 +0300

    Project

---
+++ se302/src/baglanti.java
@@ -0,0 +1,43 @@
+
+
+import java.sql.Statement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class baglanti {
+    static String url = "jdbc:sqlite:C://Users/merve/OneDrive/Belgeler/NetBeansProjects/se302/src/sql/syllabus.db";
+    static Connection getConnection(){
+        Connection conn=null;
+        try {
+            conn=DriverManager.getConnection(url);
+            System.out.println("Basarılı");
+        } catch (SQLException e) {
+            System.err.println("Başarısız");
+            
+        }
+        return conn;
+    }
+    public static void main(String[] args) {
+       
+        
+        getConnection();
+            
+            
+            /* String sql = "select * from general_information";
+            try (
+               Statement st = conn.createStatement();
+               ResultSet rs = st.executeQuery(sql)){
+               
+               while(rs.next()){
+                   System.out.println(rs.getString("c_code"));
+               }
+            } catch (Exception e) { 
+            } */
+        } 
+        
+    }
+ 
+
